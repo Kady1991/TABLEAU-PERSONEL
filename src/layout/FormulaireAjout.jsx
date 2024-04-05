798c0ee5b56ed2f91587ef01312b430acaa4589b
Kady Sangare <sangarekady663@yahoo.fr>
2024-04-05 12:40:48 +0200

    Modification logique formulaire et css

--- src/layout/FormulaireAjout.jsx
+++ src/layout/FormulaireAjout.jsx
@@ -1,22 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Input, Button, Checkbox, Select } from 'antd';
 import axios from 'axios';
-import "./FormulaireAjout.css";
-
+import './FormulaireAjout.css';
 
 const { Option } = Select;
 
 const FormulaireAjout = ({ onSubmit }) => {
   const [services, setServices] = useState([]);
   const [selectedService, setSelectedService] = useState('');
-  const [nom, setNom] = useState('');
-  const [prenom, setPrenom] = useState('');
+  const [nomPersonne, setNomPersonne] = useState('');
+  const [prenomPersonne, setPrenomPersonne] = useState('');
   const [email, setEmail] = useState('');
   const [dateEntree, setDateEntree] = useState('');
-  const [adresse, setAdresse] = useState('');
-  const [autreService, setAutreService] = useState(false);
-  const [secondServiceNom, setSecondServiceNom] = useState('');
-  const [secondServiceAdresse, setSecondServiceAdresse] = useState('');
+  const [siFrancais, setSiFrancais] = useState('');
+  const [nomRueFr, setNomRueFr] = useState('');
+  const [showSecondService, setShowSecondService] = useState(false);
+  const [secondServiceSelected, setSecondServiceSelected] = useState('');
 
   useEffect(() => {
     const fetchServices = async () => {
@@ -34,61 +33,72 @@ const FormulaireAjout = ({ onSubmit }) => {
         console.error('Erreur lors de la récupération des services:', error);
       }
     };
-  
+
     fetchServices();
   }, []);
 
   const handleSubmit = () => {
     const nouveauMembre = {
-      nom,
-      prenom,
+      nom: nomPersonne,
+      prenom: prenomPersonne,
       email,
       dateEntree,
-      adresse,
+      adresse: nomRueFr,
       service: selectedService,
+      secondService: secondServiceSelected,
     };
     onSubmit(nouveauMembre);
   };
 
-  const handleAutreServiceChange = () => {
-    setAutreService(!autreService);
+  const handleAutreServiceChange = (e) => {
+    setShowSecondService(e.target.checked);
   };
 
   return (
-    <div style={{ height: '10vh', width: '10%', position: 'absolute', top: 0 }}>
-      <Form className="formulaire">
+    <div className="formulaire">
+      <Form>
         <Form.Item label="Nom">
           <Input
-            value={nom}
-            onChange={e => setNom(e.target.value)}
+            value={nomPersonne}
+            onChange={(e) => setNomPersonne(e.target.value)}
             placeholder="Nom"
           />
         </Form.Item>
         <Form.Item label="Prénom">
           <Input
-            value={prenom}
-            onChange={e => setPrenom(e.target.value)}
+            value={prenomPersonne}
+            onChange={(e) => setPrenomPersonne(e.target.value)}
             placeholder="Prénom"
           />
         </Form.Item>
         <Form.Item label="E-mail">
           <Input
             value={email}
-            onChange={e => setEmail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="E-mail"
           />
         </Form.Item>
         <Form.Item label="Date d'entrée">
           <Input
             value={dateEntree}
-            onChange={e => setDateEntree(e.target.value)}
+            onChange={(e) => setDateEntree(e.target.value)}
             placeholder="Date d'entrée"
+          />
+        </Form.Item>
+        <Form.Item label="Langue">
+          <Checkbox.Group
+            options={[
+              { label: 'Français', value: 'FR' },
+              { label: 'Néerlandais', value: 'NL' }
+            ]}
+            value={siFrancais}
+            onChange={(values) => setSiFrancais(values)}
           />
         </Form.Item>
         <Form.Item label="Adresse">
           <Input
-            value={adresse}
-            onChange={e => setAdresse(e.target.value)}
+            value={nomRueFr}
+            onChange={(e) => setNomRueFr(e.target.value)}
             placeholder="Adresse"
           />
         </Form.Item>
@@ -97,38 +107,34 @@ const FormulaireAjout = ({ onSubmit }) => {
             placeholder="Sélectionner un service"
             style={{ width: '100%' }}
             value={selectedService}
-            onChange={value => setSelectedService(value)}
+            onChange={(value) => setSelectedService(value)}
           >
-            {services.map(service => (
+            {services.map((service) => (
               <Option key={service} value={service}>{service}</Option>
             ))}
           </Select>
         </Form.Item>
-        <Form.Item>
+        <Form.Item label="Autres services">
           <Checkbox
-            checked={autreService}
+            checked={showSecondService}
             onChange={handleAutreServiceChange}
           >
             A un autre service
           </Checkbox>
         </Form.Item>
-        {autreService && (
-          <>
-            <Form.Item label="Nom du second service">
-              <Input
-                value={secondServiceNom}
-                onChange={e => setSecondServiceNom(e.target.value)}
-                placeholder="Nom du second service"
-              />
-            </Form.Item>
-            <Form.Item label="Adresse du second service">
-              <Input
-                value={secondServiceAdresse}
-                onChange={e => setSecondServiceAdresse(e.target.value)}
-                placeholder="Adresse du second service"
-              />
-            </Form.Item>
-          </>
+        {showSecondService && (
+          <Form.Item >
+            <Select
+              placeholder="Sélectionner un service pour le second service"
+              style={{ width: '100%' }}
+              value={secondServiceSelected}
+              onChange={(value) => setSecondServiceSelected(value)}
+            >
+              {services.map((service) => (
+                <Option key={service} value={service}>{service}</Option>
+              ))}
+            </Select>
+          </Form.Item>
         )}
         <Form.Item>
           <Button type="primary" onClick={handleSubmit}>Valider</Button>
@@ -136,7 +142,6 @@ const FormulaireAjout = ({ onSubmit }) => {
       </Form>
     </div>
   );
-  
 };
 
 export default FormulaireAjout;
