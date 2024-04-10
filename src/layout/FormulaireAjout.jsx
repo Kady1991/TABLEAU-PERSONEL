9a57d27a7caeb4bbdbcd6a1a6c5b2e45ce32675a
kady <sangarekady663@yahoo.fr>
2024-04-10 15:28:13 +0200

    Mise au point des munu deroulants dans le composant formulaire ajout

--- src/layout/FormulaireAjout.jsx
+++ src/layout/FormulaireAjout.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { TextField, Button, RadioGroup, FormControlLabel, Radio, Select, MenuItem } from '@mui/material';
-import { DatePicker } from '@mui/lab';
 import { AccountCircle, AlternateEmail, LocationOn, Phone } from '@mui/icons-material';
 import axios from 'axios';
 import './FormulaireAjout.css';
@@ -12,60 +11,42 @@ const FormulaireAjout = ({ onSubmit }) => {
   const [prenomPersonne, setPrenomPersonne] = useState('');
   const [email, setEmail] = useState('');
   const [dateEntree, setDateEntree] = useState(null);
-  const [siFrancais, setSiFrancais] = useState('');
   const [nomRueFr, setNomRueFr] = useState('');
   const [telPro, setTelPro] = useState('');
   const [numeroNational, setNumeroNational] = useState('');
-  const [typePersonnel, setTypePersonnel] = useState(''); // Valeur initiale à définir
-  const [grades, setGrades] = useState([]); // Liste des grades récupérés depuis l'API
-  const [selectedGrade, setSelectedGrade] = useState(''); // Grade sélectionné dans le menu déroulant
-  const [langue, setLangue] = useState('fr'); // Langue par défaut
-  const [adresses, setAdresses] = useState([]); // Liste des adresses récupérées depuis l'API
-  const [selectedAdresse, setSelectedAdresse] = useState(''); // Adresse sélectionnée dans le menu déroulant
+  const [typePersonnel, setTypePersonnel] = useState('');
+  const [langue, setLangue] = useState('fr');
+  const [adresses, setAdresses] = useState([]);
+  const [selectedAdresse, setSelectedAdresse] = useState('');
+  const [grades, setGrades] = useState([]); // Ajout de l'état pour les grades
+  const [selectedGrade, setSelectedGrade] = useState(''); // Ajout de l'état pour le grade sélectionné
 
   useEffect(() => {
-    const fetchServices = async () => {
+    const fetchData = async () => {
       try {
         const response = await axios.get('https://server-iis.uccle.intra/API_Personne/api/Personne');
         const data = response.data;
         if (Array.isArray(data)) {
           const serviceNames = data.map(item => item.NomServiceFr);
-          const uniqueServiceNames = [...new Set(serviceNames)].sort(); // Tri alphabétique des noms de service
+          const uniqueServiceNames = [...new Set(serviceNames)].sort();
           setServices(uniqueServiceNames);
+
+          const adresses = data.map(item => item.NomRueFr);
+          const uniqueAdresses = [...new Set(adresses)].sort();
+          setAdresses(uniqueAdresses);
+
+          const nomWWGradeFr = data.map(item => item.NomWWGradeFr);
+          const uniqueNomWWGradeFr = [...new Set(nomWWGradeFr)].sort();
+          setGrades(uniqueNomWWGradeFr);
         } else {
           console.error('La réponse de l\'API n\'est pas un tableau:', data);
         }
       } catch (error) {
-        console.error('Erreur lors de la récupération des services:', error);
+        console.error('Erreur lors de la récupération des données:', error);
       }
     };
 
-    const fetchGrades = async () => {
-      try {
-        // Remplacer l'URL par celle de l'API des grades
-        const response = await axios.get('https://server-iis.uccle.intra/API_Personne/api/Personne');
-        const data = response.data;
-        setGrades(data); // Mettre à jour la liste des grades avec les données de l'API
-      } catch (error) {
-        console.error('Erreur lors de la récupération des grades:', error);
-      }
-    };
-
-    const fetchAdresses = async () => {
-      try {
-        // Remplacer l'URL par celle de l'API des adresses
-        const response = await axios.get('https://server-iis.uccle.intra/API_Personne/api/Personne');
-        const data = response.data;
-        const uniqueAdresses = [...new Set(data)].sort(); // Tri alphabétique des adresses
-        setAdresses(uniqueAdresses);
-      } catch (error) {
-        console.error('Erreur lors de la récupération des adresses:', error);
-      }
-    };
-
-    fetchServices();
-    fetchGrades();
-    fetchAdresses();
+    fetchData();
   }, []);
 
   const handleSubmit = () => {
@@ -74,13 +55,12 @@ const FormulaireAjout = ({ onSubmit }) => {
       prenom: prenomPersonne,
       email,
       dateEntree,
-      adresse: nomRueFr,
+      adresse: selectedAdresse,
       service: selectedService,
       numeroNational,
       typePersonnel,
-      grade: selectedGrade,
       langue,
-      selectedAdresse, // Utilisation de selectedAdresse à la place de nomRueFr
+      grade: selectedGrade, // Utilisation de selectedGrade pour le grade
     };
     onSubmit(nouveauMembre);
   };
@@ -112,15 +92,8 @@ const FormulaireAjout = ({ onSubmit }) => {
             startAdornment: <AlternateEmail />,
           }}
         />
-        <DatePicker
-          label="Date d'entrée"
-          value={dateEntree}
-          onChange={(date) => setDateEntree(date)}
-          renderInput={(params) => <TextField {...params} />}
-        />
-
         <TextField
-          label="Telephone"
+          label="Téléphone professionnel"
           value={telPro}
           onChange={(e) => setTelPro(e.target.value)}
           InputProps={{
@@ -132,14 +105,13 @@ const FormulaireAjout = ({ onSubmit }) => {
           value={numeroNational}
           onChange={(e) => setNumeroNational(e.target.value)}
         />
-      
         <Select
           label="Adresse"
           value={selectedAdresse}
           onChange={(e) => setSelectedAdresse(e.target.value)}
         >
-          {adresses.map((adresse) => (
-            <MenuItem key={adresse} value={adresse}>{adresse}</MenuItem>
+          {adresses.map((adresse, index) => (
+            <MenuItem key={index} value={adresse}>{adresse}</MenuItem>
           ))}
         </Select>
         <Select
@@ -147,17 +119,17 @@ const FormulaireAjout = ({ onSubmit }) => {
           value={selectedService}
           onChange={(e) => setSelectedService(e.target.value)}
         >
-          {services.map((service) => (
-            <MenuItem key={service} value={service}>{service}</MenuItem>
+          {services.map((service, index) => (
+            <MenuItem key={index} value={service}>{service}</MenuItem>
           ))}
         </Select>
         <Select
-          label="Adresse"
-          value={selectedAdresse}
-          onChange={(e) => setSelectedAdresse(e.target.value)}
+          label="Grade"
+          value={selectedGrade}
+          onChange={(e) => setSelectedGrade(e.target.value)}
         >
-          {adresses.map((adresse) => (
-            <MenuItem key={adresse} value={adresse}>{adresse}</MenuItem>
+          {grades.map((grade, index) => (
+            <MenuItem key={index} value={grade}>{grade}</MenuItem>
           ))}
         </Select>
         <div className="radio-group">
@@ -180,7 +152,6 @@ const FormulaireAjout = ({ onSubmit }) => {
             <FormControlLabel value="nl" control={<Radio />} label="Néerlandais" />
           </RadioGroup>
         </div>
-        {/* Bouton Valider */}
         <div className="valider-button">
           <Button variant="contained" onClick={handleSubmit}>Valider</Button>
         </div>
