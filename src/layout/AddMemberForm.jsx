7c8c6654a872499e2416b2618f14822b2b8b9acb
kady <sangarekady663@yahoo.fr>
2024-04-15 15:42:40 +0200

    formulaire correcte

--- src/layout/AddMemberForm.jsx
+++ src/layout/AddMemberForm.jsx
@@ -53,12 +53,22 @@ const AddMemberForm = () => {
         siPersonnel = false;
       }
 
-      // Envoyer la requête à l'API avec les valeurs mappées
-      const response = await axios.post('https://server-iis.uccle.intra/API_Personne/api/Personne', {
-        ...values,
+      // Créer un objet avec les valeurs du formulaire
+      const formData = {
+        NomPersonne: values.nom,
+        PrenomPersonne: values.prenom,
+        Email: values.email,
+        TelPro: values.telephone,
+        DateEntree: values.dateEntree,
+        Grade: values.grade,
+        Adresse: values.adresse,
+        Service: values.service,
         SiFrancais: siFrancais,
         SiTypePersonnel: siPersonnel ? 'Oui' : 'Non',
-      });
+      };
+
+      // Envoyer la requête PUT à l'API avec les données du formulaire
+      const response = await axios.put('https://server-iis.uccle.intra/API_Personne/api/Personne', formData);
 
       if (!response.ok) {
         throw new Error('Erreur lors de l\'envoi des données');
@@ -82,11 +92,11 @@ const AddMemberForm = () => {
         <p>Chargement des données...</p>
       ) : (
         <Form
-        form={form}
-        layout="vertical"
-        onFinish={handleSubmit}
-        style={{ maxWidth: '600px', width: '100%', padding: '20px', backgroundColor: '#f0f2f5', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0,0,0,0.1)' }}
-      >
+          form={form}
+          layout="vertical"
+          onFinish={handleSubmit}
+          style={{ maxWidth: '600px', width: '100%', padding: '20px', backgroundColor: '#f0f2f5', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0,0,0,0.1)' }}
+        >
           <Row gutter={16}>
             <Col span={12}>
               <Form.Item
@@ -167,62 +177,60 @@ const AddMemberForm = () => {
               </Form.Item>
             </Col>
 
-            
             <Col span={12}>
-    <Form.Item
-      name="grade"
-      label="Grade"
-      rules={[{ required: true, message: 'Veuillez choisir le grade' }]}
-    >
-      <Select style={{ width: '100%' }}>
-        <option value="">Sélectionner un grade</option>
-        {[...new Set(grades.map(grade => grade.NomWWGradeFr))]
-          .sort()
-          .map((gradeName, index) => (
-            <option key={index} value={gradeName}>{gradeName}</option>
-          ))}
-      </Select>
-    </Form.Item>
-  </Col>
-          </Row>
-
-          <Row gutter={16}>
-          <Col span={12}>
-    <Form.Item
-      name="adresse"
-      label="Adresse"
-      rules={[{ required: true, message: 'Veuillez choisir l\'adresse' }]}
-    >
-      <Select style={{ width: '100%' }}>
-        <option value="">Sélectionner une adresse</option>
-        {[...new Set(addressData.map(address => address.NomRueFr))]
-          .sort()
-          .map((addressName, index) => (
-            <option key={index} value={addressName}>{addressName}</option>
-          ))}
-      </Select>
-    </Form.Item>
-  </Col>
-  <Col span={12}>
               <Form.Item
-                name="service"
-                label="Service"
-                
-                rules={[{ required: true, message: 'Veuillez choisir le service' }]}
+                name="grade"
+                label="Grade"
+                rules={[{ required: true, message: 'Veuillez choisir le grade' }]}
               >
                 <Select style={{ width: '100%' }}>
-        <option value="">Sélectionner un service</option>
-        {[...new Set(services.map(service => service.NomServiceFr))]
-          .sort()
-          .map((serviceName, index) => (
-            <option key={index} value={serviceName}>{serviceName}</option>
-          ))}
-      </Select>
+                  <Option value="">Sélectionner un grade</Option>
+                  {[...new Set(grades.map(grade => grade.NomWWGradeFr))]
+                    .sort()
+                    .map((gradeName, index) => (
+                      <Option key={index} value={gradeName}>{gradeName}</Option>
+                    ))}
+                </Select>
               </Form.Item>
             </Col>
           </Row>
 
-          <Form.Item style={{ textAlign: 'center', fontSize:"5rem", }}>
+          <Row gutter={16}>
+            <Col span={12}>
+              <Form.Item
+                name="adresse"
+                label="Adresse"
+                rules={[{ required: true, message: 'Veuillez choisir l\'adresse' }]}
+              >
+                <Select style={{ width: '100%' }}>
+                  <Option value="">Sélectionner une adresse</Option>
+                  {[...new Set(addressData.map(address => address.NomRueFr))]
+                    .sort()
+                    .map((addressName, index) => (
+                      <Option key={index} value={addressName}>{addressName}</Option>
+                    ))}
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item
+                name="service"
+                label="Service"
+                rules={[{ required: true, message: 'Veuillez choisir le service' }]}
+              >
+                <Select style={{ width: '100%' }}>
+                  <Option value="">Sélectionner un service</Option>
+                  {[...new Set(services.map(service => service.NomServiceFr))]
+                    .sort()
+                    .map((serviceName, index) => (
+                      <Option key={index} value={serviceName}>{serviceName}</Option>
+                    ))}
+                </Select>
+              </Form.Item>
+            </Col>
+          </Row>
+
+          <Form.Item style={{ textAlign: 'center' }}>
             <Button type="primary" htmlType="submit" loading={loading}>
               Valider
             </Button>
