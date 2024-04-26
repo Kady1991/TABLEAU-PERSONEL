b91a982d60e130fd842889ffbd4f998887e196bc
kady <sangarekady663@yahoo.fr>
2024-04-26 11:32:57 +0200

    post

--- src/components/Tableau.jsx
+++ src/components/Tableau.jsx
@@ -5,6 +5,8 @@ import Export from "../layout/Export"; // Importez votre composant ButtonExport 
 import ActionComponent from "../layout/ActionComponent";
 import { Fab } from '@mui/material';
 import { Add as AddIcon } from '@mui/icons-material';
+import "../index.css"; // Importez le fichier CSS
+import styled from 'styled-components';
 
 function Tableau() {
   const [personnes, setPersonnes] = useState([]);
@@ -42,36 +44,13 @@ function Tableau() {
 
 
   const columns = [
-    { field: "IDPersonne", headerName: "ID", width: 50, hideable: true },
-    { field: "NomPersonne", headerName: "NOM", width: 150, },
-    { field: "PrenomPersonne", headerName: "PRENOM", width: 150 },
-    { field: "Email", headerName: "E-mail", width: 250 },
-    { field: "TelPro", headerName: "TEL", width: 150 },
-    { field: "SiFrancais", headerName: "RÔLE", width: 100, hide: true },
-    { field: "DateEntree", headerName: "ENTREE SERVICE", width: 250, hide: true },
-    { field: "NomWWGradeNl", headerName: "GRADE(nl)", width: 150, hide: true },
-    { field: "NomWWGradeFr", headerName: "GRADE", width: 150, hide: true },
-    { field: "NomServiceNl", headerName: "AFFECTATION (nl)", width: 150, hide: true },
-    { field: "NomServiceFr", headerName: "AFFECTATION", width: 150, hide: true },
-    { field: "NomRueNl", headerName: "LOCALISATION(nl)", width: 150, hide: true },
-    { field: "NomRueFr", headerName: "LOCALISATION", width: 150, hide: true },
-    { field: "Numero", headerName: "N°", width: 100, hide: true },
-    { field: "NomChefService", headerName: "NOM CHEF DU SERVICE", width: 250, hide: true },
-    { field: "PrenomChefService", headerName: "PRENOM CHEF DU SERVICE", width: 250, hide: true },
-    { field: "EmailChefService", headerName: "E-MAIL CHEF DU SERVICE", width: 250, hide: true },
-    { field: "NomDepartementNl", headerName: "DEPARTEMENT(nl)", width: 250, hide: true },
-    { field: "NomDepartementFr", headerName: "DEPARTEMENTS", width: 250, hide: true },
-    { field: "NomChefDepartement", headerName: "NOM CHEF DEPARTEMENT", width: 250, hide: true },
-    { field: "PrenomChefDepartement", headerName: "PRENOM CHEF DEPARTEMENT", width: 250, hide: true },
-    { field: "EmailChefDepartement", headerName: "E-MAIL CHEF DEPARTEMENT", width: 250, hide: true },
-    { field: "P+C:UENSION", headerName: "P+C:UENSION", width: 100, hide: true },
-    { field: "Batiment", headerName: "Batiment", width: 100, hide: true },
-    { field: "Etage", headerName: "Etage", width: 100, hide: true },
-    { field: "BatimentNl", headerName: "Batiment(nl)", width: 100, hide: true },
+   
+    { field: "IDPersonne", headerName: "ID", width: 150, hideable: true },
     {
       field: 'actions',
-      headerName: 'Actions',
-      width: 200,
+      headerName: 'Actions' ,
+      width: 250, 
+
       sortable: false,
       renderCell: (params) => (
         <ActionComponent rowData={params.row} 
@@ -83,6 +62,32 @@ function Tableau() {
       ),
       
     },
+    { field: "NomPersonne", headerName: "NOM", width: 200, },
+    { field: "PrenomPersonne", headerName: "PRENOM", width: 150 },
+    { field: "Email", headerName: "E-mail", width: 250 },
+    { field: "TelPro", headerName: "TEL", width: 150 },
+    { field: "SiFrancais", headerName: "RÔLE", width: 100, hide: true },
+    { field: "DateEntree", headerName: "ENTREE SERVICE", width: 200, hide: true },
+    { field: "NomWWGradeNl", headerName: "GRADE(nl)", width: 200, hide: true },
+    { field: "NomWWGradeFr", headerName: "GRADE", width: 200, hide: true },
+    { field: "NomServiceNl", headerName: "AFFECTATION (nl)", width: 250, hide: true },
+    { field: "NomServiceFr", headerName: "AFFECTATION", width: 250, hide: true },
+    { field: "NomRueNl", headerName: "LOCALISATION(nl)", width: 250, hide: true },
+    { field: "NomRueFr", headerName: "LOCALISATION", width: 250, hide: true },
+    { field: "Numero", headerName: "N°", width: 100, hide: true },
+    { field: "NomChefService", headerName: "NOM CHEF DU SERVICE", width: 250, hide: true },
+    { field: "PrenomChefService", headerName: "PRENOM CHEF DU SERVICE", width: 250, hide: true },
+    { field: "EmailChefService", headerName: "E-MAIL CHEF DU SERVICE", width: 250, hide: true },
+    { field: "NomDepartementNl", headerName: "DEPARTEMENT(nl)", width: 250, hide: true },
+    { field: "NomDepartementFr", headerName: "DEPARTEMENTS", width: 250, hide: true },
+    { field: "NomChefDepartement", headerName: "NOM CHEF DEPARTEMENT", width: 250, hide: true },
+    { field: "PrenomChefDepartement", headerName: "PRENOM CHEF DEPARTEMENT", width: 250, hide: true },
+    { field: "EmailChefDepartement", headerName: "E-MAIL CHEF DEPARTEMENT", width: 250, hide: true },
+    { field: "P+C:UENSION", headerName: "P+C:UENSION", width: 150, hide: true },
+    { field: "Batiment", headerName: "Batiment", width: 100, hide: true },
+    { field: "Etage", headerName: "Etage", width: 100, hide: true },
+    { field: "BatimentNl", headerName: "Batiment(nl)", width: 100, hide: true },
+  
   ];
 
 
@@ -134,7 +139,7 @@ function Tableau() {
   
       {/* Affichage conditionnel du formulaire */}
       {showForm && (
-        <div style={{ margin: "2rem" }}>
+        <div style={{ }}>
           <AddMemberForm />
         </div>
       )}
--- src/layout/AddMemberForm.jsx
+++ src/layout/AddMemberForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { Form, Input, DatePicker, Select, Button, Row, Col, Radio } from "antd";
 import axios from "axios";
 
