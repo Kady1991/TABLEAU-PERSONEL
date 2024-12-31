c3ab8e3558dcaf6615435e45c9aaf24a0e19f55c
Kady Sangare <ksangare@uccle.brussels>
2024-12-31 13:56:34 +0100

    modifications de l'affichage dans les statistiques

--- src/layout/Statistics.jsx
+++ src/layout/Statistics.jsx
@@ -13,11 +13,10 @@ const Statistics = ({ onClose }) => {
   const [yearData, setYearData] = useState([]);
   const [selectedYear, setSelectedYear] = useState(2024);
   const [selectedDepartment, setSelectedDepartment] = useState('');
-  const [selectedService, setSelectedService] = useState(''); // Service sélectionné
   const [departments, setDepartments] = useState([]);
-  const [services, setServices] = useState([]); // Liste des services
   const [loading, setLoading] = useState(false);
   const [globalTotals, setGlobalTotals] = useState({ totalEntries: 0, totalExits: 0 });
+  const [chartXAxis, setChartXAxis] = useState([]);
 
   const fetchDepartments = async () => {
     try {
@@ -26,7 +25,7 @@ const Statistics = ({ onClose }) => {
 
       const uniqueDepartments = Array.from(
         new Set(persons.map((person) => person.NomDepartementFr))
-      ).map((department) => ({ NomDepartementFr: department }));
+      );
 
       setDepartments(uniqueDepartments);
     } catch (error) {
@@ -34,22 +33,7 @@ const Statistics = ({ onClose }) => {
     }
   };
 
-  const fetchServices = async () => {
-    try {
-      const response = await axios.get('https://server-iis.uccle.intra/API_PersonneTest/api/Personne');
-      const persons = response.data;
-
-      const uniqueServices = Array.from(
-        new Set(persons.map((person) => person.NomServiceFr))
-      ).map((service) => ({ NomServiceFr: service }));
-
-      setServices(uniqueServices);
-    } catch (error) {
-      console.error("Erreur lors de la récupération des services:", error);
-    }
-  };
-
-  const fetchData = async (year, department, service) => {
+  const fetchData = async (year, department) => {
     setLoading(true);
     const parser = new XMLParser();
 
@@ -59,24 +43,27 @@ const Statistics = ({ onClose }) => {
       );
       const persons = personsResponse.data;
 
-      const monthStats = Array(12).fill({ entries: 0, exits: 0 }).map(() => ({ entries: 0, exits: 0 }));
+      const stats = {};
       let totalEntries = 0;
       let totalExits = 0;
 
       for (const person of persons) {
-        if (department && person.NomDepartementFr !== department) {
+        const personDepartment = person.NomDepartementFr;
+        const service = person.NomServiceFr;
+
+        if (department && personDepartment !== department) {
           continue;
         }
 
-        if (service && person.NomServiceFr !== service) {
-          continue;
+        if (!stats[service]) {
+          stats[service] = { entries: 0, exits: 0 };
         }
 
         if (person.DateEntree) {
           const entryDate = dayjs(person.DateEntree);
           totalEntries++;
           if (entryDate.year() === year) {
-            monthStats[entryDate.month()].entries++;
+            stats[service].entries++;
           }
         }
 
@@ -94,7 +81,7 @@ const Statistics = ({ onClose }) => {
             if (exitDate.isValid()) {
               totalExits++;
               if (exitDate.year() === year) {
-                monthStats[exitDate.month()].exits++;
+                stats[service].exits++;
               }
             }
           } catch (error) {
@@ -103,7 +90,11 @@ const Statistics = ({ onClose }) => {
         }
       }
 
-      setYearData(monthStats);
+      setYearData(Object.values(stats));
+      setChartXAxis(department ? Object.keys(stats) : [
+        'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
+        'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'
+      ]);
       setGlobalTotals({ totalEntries, totalExits });
     } catch (error) {
       console.error("Erreur lors de la récupération des données:", error);
@@ -114,33 +105,24 @@ const Statistics = ({ onClose }) => {
 
   useEffect(() => {
     fetchDepartments();
-    fetchServices();
   }, []);
 
   useEffect(() => {
-    fetchData(selectedYear, selectedDepartment, selectedService);
-  }, [selectedYear, selectedDepartment, selectedService]);
-
-  const months = [
-    'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
-    'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'
-  ];
+    fetchData(selectedYear, selectedDepartment);
+  }, [selectedYear, selectedDepartment]);
 
   const series = [
     {
       label: 'Entrées',
-      data: yearData.map((month) => month.entries),
-      color: '#649B88', // Couleur pour les entrées (vert)
+      data: yearData.map((data) => data.entries),
+      color: '#649B88',
     },
     {
       label: 'Sorties',
-      data: yearData.map((month) => month.exits),
-      color: '#AE4A34', // Couleur pour les sorties (rouge)
+      data: yearData.map((data) => data.exits),
+      color: '#AE4A34',
     },
   ];
-
-  const totalEntries = yearData.reduce((acc, month) => acc + month.entries, 0);
-  const totalExits = yearData.reduce((acc, month) => acc + month.exits, 0);
 
   return (
     <div className="statistics-container" style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100%' }}>
@@ -152,19 +134,18 @@ const Statistics = ({ onClose }) => {
           color: 'red',
           fontSize: '2rem',
           position: 'absolute',
-          right: '20px', // Positionnez-le à gauche
-          top: '20px', // Optionnel, ajustez pour aligner verticalement
+          left: '20px',
+          top: '20px',
           transition: 'color 0.3s ease',
         }}
-        onMouseEnter={(e) => (e.target.style.color = 'darkred')} // Changement de couleur au survol
-        onMouseLeave={(e) => (e.target.style.color = 'red')} // Retour à la couleur rouge après survol
+        onMouseEnter={(e) => (e.target.style.color = 'darkred')}
+        onMouseLeave={(e) => (e.target.style.color = 'red')}
       />
-
 
       {loading ? (
         <p>Chargement des données...</p>
       ) : (
-        <Box sx={{ width: '80%', padding: '20px', flex: '1', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Box sx={{ width: '70%', padding: '20px', flex: '1', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: '20px', justifyContent: 'space-between', width: '100%' }}>
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
               <TextField
@@ -185,60 +166,35 @@ const Statistics = ({ onClose }) => {
                 size="small"
                 style={{ marginLeft: '20px', width: '300px' }}
                 SelectProps={{ native: true }}
-                InputLabelProps={{ shrink: true }} // Ajout pour que le label reste fixe
+                InputLabelProps={{ shrink: true }}
               >
                 <option value="">Tous les départements</option>
                 {departments.map((dept, index) => (
-                  <option key={index} value={dept.NomDepartementFr}>{dept.NomDepartementFr}</option>
-                ))}
-              </TextField>
-              <TextField
-                select
-                label="Service"
-                value={selectedService}
-                onChange={(event) => setSelectedService(event.target.value)}
-                variant="outlined"
-                size="small"
-                style={{ marginLeft: '20px', width: '300px' }}
-                SelectProps={{ native: true }}
-                InputLabelProps={{ shrink: true }}
-              >
-                <option value="">Tous les services</option>
-                {services.map((service, index) => (
-                  <option key={index} value={service.NomServiceFr}>{service.NomServiceFr}</option>
+                  <option key={index} value={dept}>{dept}</option>
                 ))}
               </TextField>
             </Box>
             <div style={{ display: 'block', fontSize: '16px', fontWeight: 'bold' }}>
-              <div className='div-entree-annee'>
-                <div style={{ color: '#4caf50', marginBottom: '5px', fontSize: '1.2rem' }}>Entrées personnel (Année): {totalEntries}</div>
-                <div style={{ color: '#f44336', marginBottom: '5px', fontSize: '1.2rem'  }}>Sorties personnel (Année): {totalExits}</div>
-              </div>
-
-              <div className='div-sortie-total'>
-                <div style={{ color: '#4caf50', marginBottom: '5px', fontSize: '1.2rem'  }}>Total Global du personnel: {globalTotals.totalEntries}</div>
-                <div style={{ color: '#f44336', fontSize: '1.2rem'  }}>Total des Sorties: {globalTotals.totalExits}</div>
-              </div>
-
-
+              <div style={{ color: '#649B88', marginBottom: '5px', fontSize: '1.2rem' }}>Personnel total present : {globalTotals.totalEntries}</div>
+              <div style={{ color: '#AE4A34', marginBottom: '5px', fontSize: '1.2rem' }}>Sorties total personnel : {globalTotals.totalExits}</div>
             </div>
           </Box>
 
           <BarChart
             height={350}
-            xAxis={[{ scaleType: 'band', data: months }]}
+            xAxis={[{ scaleType: 'band', data: chartXAxis }]}
             series={series}
           />
         </Box>
       )}
 
-      <div style={{ width: '100%', flex: '1', display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20px' }}>
+      <div style={{ width: '50%', flex: '1', display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '20px', marginTop:'10'}}>
         <PieChart
           series={[
             {
               data: [
-                { value: totalEntries, label: 'Total Entrées', color: '#649B88' },
-                { value: totalExits, label: 'Total Sorties', color: '#AE4A34' },
+                { value: globalTotals.totalEntries, label: 'Total Entrées', color: '#649B88' },
+                { value: globalTotals.totalExits, label: 'Total Sorties', color: '#AE4A34' },
               ],
               highlightScope: { fade: 'global', highlight: 'item' },
               faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
@@ -252,4 +208,3 @@ const Statistics = ({ onClose }) => {
 };
 
 export default Statistics;
-
