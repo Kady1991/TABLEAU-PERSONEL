b57ccce11ad98574c463218743ad9b5e7470f946
nachoukevitch <nachoukevitch@LAPTOP-U002>
2025-02-12 13:07:09 +0100

    change le lien d'application

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const LIEN_API_PERSONNE = "https://server-iis.uccle.intra/API_PersonneTest";
+//export const LIEN_API_PERSONNE = "https://server-iis.uccle.intra/API_PersonneTest";
+export const LIEN_API_PERSONNE = "https://server-iis.uccle.intra/APIPersonnelUccle";
