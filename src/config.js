ec555e431ecb2a1e2291b97d9f40c12d58500728
nachoukevitch <nachoukevitch@LAPTOP-U002>
2026-03-05 15:00:49 +0100

    liens

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 //export const LIEN_API_PERSONNE = "https://server-iis.uccle.intra/API_PersonneTest";
-export const LIEN_API_PERSONNE = "https://server-iis.uccle.intra/APIPersonnelUccle";
+export const LIEN_API_PERSONNE =
+  "https://server-iis.uccle.intra/APIPersonnelUccleTest";
--- src/services/PersonnelService.js
+++ src/services/PersonnelService.js
@@ -1,36 +1,33 @@
 import axios from "axios";
 import { LIEN_API_PERSONNE } from "../config";
 
-
 const http = axios.create({
   baseURL: LIEN_API_PERSONNE,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  // headers: {
+  //   "Content-Type": "application/json",
+  // },
   // Décommente si nécessaire pour IIS / SSO
-  // withCredentials: true,
+  withCredentials: true,
 });
 
 const PersonnelService = {
   // Récupérer tous les personnels
-  getAll: () => http.get("/api/Personne"),
+  getAll: () => http.get("/api/Personnes/liste"),
 
   // Récupérer un personnel par ID
   getById: (id) => http.get(`/api/Personne/${id}`),
 
   //Modifier un personnel
-  update: (id, payload) =>
-    http.put(`/api/personne/edit?id=${id}`, payload),
+  update: (id, payload) => http.put(`/api/personne/edit?id=${id}`, payload),
 
   // Restaurer un personnel archivé
-  restore: (id) =>
-    http.put(`/api/personne/desarchiver?id=${id}`),
+  restore: (id) => http.put(`/api/personne/desarchiver?id=${id}`),
 
   // Référentiels
-  getGrades: () => http.get("/api/wwgrades"),
-  getAdresses: () => http.get("/api/Adresses"),
-  getTypesPersonnel: () => http.get("/api/typepersonnel"),
-  getServices: () => http.get("/api/affectation/services"),
+  getGrades: () => http.get("/api/infos/wwgrades"),
+  getAdresses: () => http.get("/api/infos/Adresses"),
+  getTypesPersonnel: () => http.get("/api/infos/typepersonnel"),
+  getServices: () => http.get("/api/infos/services"),
 };
 
 export default PersonnelService;
